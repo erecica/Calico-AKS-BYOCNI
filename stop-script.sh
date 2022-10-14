e5cc20a85f2075967588d1dfcc4ea1d6b6fdd492
Edi Recica <edi@recica.nl>
2022-10-14 13:14:56 +0200

    initial commit

---
+++ stop-script.sh
@@ -0,0 +1,6 @@
+az aks delete --name Calico-AKS-Workshop --resource-group Calico-AKS-Resourcegroup -y && \
+az group delete --resource-group Calico-AKS-Resourcegroup -y && \
+kubectl config delete-cluster Calico-AKS-Workshop && \
+kubectl config delete-context Calico-AKS-Workshop && \
+kubectl config delete-user clusterUser_Calico-AKS-Resourcegroup_Calico-AKS-Workshop && \
+clouddrive unmount
---
+++ strart-script.sh
@@ -0,0 +1,8 @@
+az group create --name Calico-AKS-Resourcegroup --location  westeurope && \
+az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --location westeurope --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys && \
+az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop && \
+kubectl create -f ./tigera-operator.yaml && \
+kubectl create -f ./tigera-operator-installation.yaml && \
+kubectl apply -f ./yaobank-org.yaml && \
+kubectl apply -f yoabank-loadbalancer.yaml && \
+kubectl get svc -n yaobank-customer yaobank-customer -w

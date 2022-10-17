c8b1e76966a35449e6b7eccf09ca7630bad2e99f
Edi Recica <edi@recica.nl>
2022-10-17 16:53:18 +0200

    shorter sleep

--- start-script.sh
+++ start-script.sh
@@ -1,22 +1,22 @@
 az group create --name Calico-AKS-Resourcegroup --location  westeurope --debug && \
-echo "Resourcegroup created. Sleep for 10 sec" && \
-sleep 10 && \ 
+echo "Resourcegroup created. Sleep for 5 sec" && \
+sleep 5 && \ 
 az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --location westeurope --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys --debug && \
-echo "AKS without network-plugin created. Sleep for 10 sec" && \
-sleep 10 && \
+echo "AKS without network-plugin created. Sleep for 5 sec" && \
+sleep 5 && \
 az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --debug && \
-echo "Get AKS credentials. Sleep for 10 sec" && \
-sleep 10 && \
+echo "Get AKS credentials. Sleep for 5 sec" && \
+sleep 5 && \
 kubectl create -f ./tigera-operator.yaml && \
-echo "Create operator. Sleep for 10 sec" && \
-sleep 10 && \
+echo "Create operator. Sleep for 5 sec" && \
+sleep 5 && \
 kubectl create -f ./tigera-operator-installation.yaml && \
-echo "Install operator. Sleep for 10 sec" && \
-sleep 10 && \
+echo "Install operator. Sleep for 5 sec" && \
+sleep 5 && \
 kubectl apply -f ./yaobank-org.yaml && \
-echo "Deploye YOABank app. Sleep for 10 sec" && \
-sleep 10 && \
+echo "Deploye YOABank app. Sleep for 5 sec" && \
+sleep 5 && \
 kubectl apply -f ./yoabank-loadbalancer.yaml && \
-echo "Deploye Loadbalancer. Sleep for 10 sec" && \
-sleep 10 && \
+echo "Deploye Loadbalancer. Sleep for 5 sec" && \
+sleep 5 && \
 kubectl get svc -n yaobank-customer yaobank-customer -w

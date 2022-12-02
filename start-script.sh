8581877f6615dc83c400e1e764afc8ebc30f6ed3
Edi Recica <edi@recica.nl>
2022-12-02 14:13:48 +0100

    remove debug code

--- start-script.sh
+++ start-script.sh
@@ -1,13 +1,13 @@
 WCO='\033[0;32m' # With color
 NC='\033[0m' # No Color
 
-az group create --name Calico-AKS-Resourcegroup --debug && \
+az group create --name Calico-AKS-Resourcegroup && \
 echo "${WCO}Resourcegroup created.\nMoving to the next step${NC}" && \
 sleep 5 && \ 
-az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys --debug && \
+az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys && \
 echo "${WCO}AKS without network-plugin created.\nThis might take 6-9 minutes${NC}" && \
 sleep 5 && \
-az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --debug && \
+az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop && \
 echo "${WCO}Get AKS credentials.${NC}" && \
 sleep 5 && \
 kubectl create -f ./tigera-operator.yaml && \

53c56f058ea647a271fbdd7191d472bf802f1f2c
Edi Recica <edi@recica.nl>
2022-12-02 14:14:58 +0100

    remove debug

--- cleanup-script.sh
+++ cleanup-script.sh
@@ -1,5 +1,5 @@
-az aks delete --name Calico-AKS-Workshop --resource-group Calico-AKS-Resourcegroup --debug -y && \
-az group delete --resource-group Calico-AKS-Resourcegroup --debug -y && \
+az aks delete --name Calico-AKS-Workshop --resource-group Calico-AKS-Resourcegroup -y && \
+az group delete --resource-group Calico-AKS-Resourcegroup -y && \
 kubectl config delete-cluster Calico-AKS-Workshop && \
 kubectl config delete-context Calico-AKS-Workshop && \
 kubectl config delete-user clusterUser_Calico-AKS-Resourcegroup_Calico-AKS-Workshop && \

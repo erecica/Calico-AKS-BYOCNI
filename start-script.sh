7a67d26642ee0479fecabade174ab11f25472647
Edi Recica <edi@recica.nl>
2022-12-02 14:20:49 +0100

    remove color from script

--- start-script.sh
+++ start-script.sh
@@ -1,25 +1,22 @@
-WCO='\033[0;32m' # With color
-NC='\033[0m' # No Color
-
-az group create --name Calico-AKS-Resourcegroup && \
-echo "${WCO}Resourcegroup created.\nMoving to the next step${NC}" && \
+az group create --name Calico-AKS-Resourcegroup --debug && \
+echo "Resourcegroup created. Moving to the next step" && \
 sleep 5 && \ 
-az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys && \
-echo "${WCO}AKS without network-plugin created.\nThis might take 6-9 minutes${NC}" && \
+az aks create --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --pod-cidr 192.168.0.0/16 --network-plugin none --generate-ssh-keys --debug && \
+echo "AKS without network-plugin created. This might take 6-9 minutes" && \
 sleep 5 && \
-az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop && \
-echo "${WCO}Get AKS credentials.${NC}" && \
+az aks get-credentials --resource-group Calico-AKS-Resourcegroup --name Calico-AKS-Workshop --debug && \
+echo "Get AKS credentials." && \
 sleep 5 && \
 kubectl create -f ./tigera-operator.yaml && \
-echo "${WCO}Create operator.\nOne moment please...${NC}" && \
+echo "Create operator. One moment please..." && \
 sleep 5 && \
 kubectl create -f ./tigera-operator-installation.yaml && \
-echo "${WCO}Install operator.\nOne moment please...${NC}" && \
+echo "Install operator. One moment please..." && \
 sleep 5 && \
 kubectl apply -f ./yaobank-org.yaml && \
-echo "${WCO}Deploy YOABank app. Sleep for 5 sec${NC}" && \
+echo "Deploy YOABank app. Sleep for 5 sec" && \
 sleep 5 && \
 kubectl apply -f ./yoabank-loadbalancer.yaml && \
-echo "${WCO}Deploy Loadbalancer. Waiting for External IP...${NC}" && \
+echo "Deploy Loadbalancer. Waiting for External IP..." && \
 sleep 5 && \
 watch kubectl get svc -n yaobank-customer yaobank-customer
